47c4ff0949ed5b46dd6c01d3dc469f38671ed211
kevintm78 <kevintm78@gmail.com>
2017-12-18 08:37:04 -0500

    fix test script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_trlte_vzw_defconfig SELINUX_DEFCONFIG=selinux_defconfig
@@ -11,5 +11,5 @@ cp output/arch/arm/boot/zImage $(pwd)/AIK/split_img/boot.img-zImage
 ./tools/dtbTool -o ./AIK/split_img/boot.img-dtb -s 4096 -p ./output/scripts/dtc/ ./output/arch/arm/boot/dts/
 ./AIK/repackimg.sh
 mv ./AIK/image-new.img ./boot.img
-rm ./AIK/split_img/boot.img-dtb
-rm ./AIK/split_img/boot.img-zImage
+#rm ./AIK/split_img/boot.img-dtb
+#rm ./AIK/split_img/boot.img-zImage
